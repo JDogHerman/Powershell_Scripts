7192484a1fdd86f3a43d18b8c6730d39d961e2a9
Justin Herman <Justin6@gmail.com>
2015-01-30 16:54:51 -0500

    Update and rename disable-ausers.ps1 to Disable-AdUsers.ps1

---
+++ Disable-AdUsers.ps1
@@ -0,0 +1,9 @@
+﻿#this script will delete a list of users from a text file supplied in $path
+
+$path = "C:\script\disabledusers521.txt"
+
+Get-Content -Path $path | Foreach-Object {
+
+Set-ADUser -Enabled $false -Identity $_ -PassThru
+
+}
