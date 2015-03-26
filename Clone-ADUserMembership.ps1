2d553721e18c604f39a0dc5a6094f13e1cbad26e
Justin <justin.herman@altercareonline.net>
2015-03-26 10:26:05 -0400

    Updated with flow control

--- Clone-ADUserMembership.ps1
+++ Clone-ADUserMembership.ps1
@@ -1,5 +1,30 @@
-﻿#This sctipt will clone a users membership to other users
+﻿#This sctipt will clone a users (donor) membership and apply it to other user(s) (recipiants) I think it should work on multiple recipiants but I havent checked.
+$donorcheck = $false
+$recipiantscheck = $false
 
-$donor = ''
-$recipiants = ''
-Get-ADUser -Identity $donor -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members $recipiants
+while ($donorcheck -eq $false) {
+$donor = read-host "What username do you want to clone?"
+$User = Get-ADUser -LDAPFilter "(sAMAccountName=$donor)"
+If ($User -eq $Null) {"$donor does not exist in AD"}
+Else {"$donor found in AD"
+    $donorcheck = $true
+    }
+}
+
+while ($recipiantscheck -eq $false) {
+    $recipiants = read-host "Whom do you want $donor membership applied to?"
+    $User = Get-ADUser -LDAPFilter "(sAMAccountName=$recipiants)"
+    If ($User -eq $Null) {"$recipiants does not exist in AD"}
+    Else {"$recipiants found in AD"
+        $recipiantscheck = $true
+        }
+}
+
+$confirm = 'Y'
+$confirm = read-host "Are you sure you want to clone $donor to $recipiants (Y/n)?"
+if (($confirm -eq 'Y') -or ($confirm -eq 'y')) {
+    write-host "Cloning $donor to $recipiants"
+    Get-ADUser -Identity $donor -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members $recipiants
+} else {
+    write-host "You did not enter Y or y, Exiting..."
+}
